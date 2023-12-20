617dd128fedab34491597c32eec0928166640337
nkrapp <nico.krapp@suse.com>
2023-12-20 16:26:36 +0100

    day 14 attempt - unfinished

---
+++ day14/src/bin/day14-1.rs
@@ -0,0 +1,8 @@
+//count number of round rocks in each row
+// for each rock count the number of spaces until a #
+// count round rocks on the way
+// add number of spaces - round rocks in the way to the line number from the bottom
+
+fn main() {
+    return ();
+}
